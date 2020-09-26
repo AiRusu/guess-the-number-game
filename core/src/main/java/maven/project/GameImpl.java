03db2f1b8959ec6481104c81296f63c74ef1d0f7
AiRusu <aida.rusu93@gmail.com>
2020-09-26 20:29:06 +0300

    Finish game logic

--- core/src/main/java/maven/project/GameImpl.java
+++ core/src/main/java/maven/project/GameImpl.java
@@ -2,6 +2,10 @@ package maven.project;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 public class GameImpl implements Game {
 
@@ -9,6 +13,7 @@ public class GameImpl implements Game {
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
     // == fields ==
+    @Autowired
     private NumberGenerator numberGenerator;
     private int guessCount = 10;
     private int number;
@@ -18,7 +23,8 @@ public class GameImpl implements Game {
     private int remainingGuesses;
     private boolean validNumberRange = true;
 
-    // == public methods ==
+    // == init ==
+    @PostConstruct
     @Override
     public void reset() {
         smallest = 0;
@@ -29,6 +35,11 @@ public class GameImpl implements Game {
         log.debug("the number is {}", number);
     }
 
+    @PreDestroy
+    public void preDestroy(){
+        log.info("in Game preDestroy");
+    }
+    // == public methods ==
     @Override
     public int getNumber() {
         return number;
@@ -62,6 +73,19 @@ public class GameImpl implements Game {
     @Override
     public void check() {
 
+        checkValidNumberRange();
+
+        if(validNumberRange) {
+            if(guess > number) {
+                biggest = guess - 1;
+            }
+
+            if(guess < number) {
+                smallest = guess + 1;
+            }
+        }
+
+        remainingGuesses--;
     }
 
     @Override
@@ -77,5 +101,10 @@ public class GameImpl implements Game {
     @Override
     public boolean isGameLost() {
         return !isGameWon() && remainingGuesses <= 0;
+    }
+
+    // == private methods ==
+    private void checkValidNumberRange() {
+        validNumberRange = (guess >= smallest) && (guess <= biggest);
     }
 }
