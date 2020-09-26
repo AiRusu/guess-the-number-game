2fa888353b823e98c92efb3f757a203a766cf86e
AiRusu <aida.rusu93@gmail.com>
2020-09-26 20:29:58 +0300

    Finish game logic

---
+++ core/src/main/java/maven/project/AppConfig.java
@@ -0,0 +1,26 @@
+package maven.project;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "maven.project")
+public class AppConfig {
+
+    // == bean methods ==
+    @Bean
+    public NumberGenerator numberGenerator(){
+        return new NumberGeneratorImpl();
+    }
+
+    @Bean
+    public Game game(){
+        return new GameImpl();
+    }
+
+    @Bean
+    public MessageGenerator messageGenerator() {
+        return new MessageGeneratorImpl();
+    }
+}
